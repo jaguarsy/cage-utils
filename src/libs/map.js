096b7ff30f00b5dfa71fc2c9c1c247d51a1f65cb
JohnnyCage <shangyu@cheyaoshi.com>
2016-04-22 17:44:42 +0800

    map逻辑优化

--- src/libs/map.js
+++ src/libs/map.js
@@ -7,22 +7,19 @@ var chainResult = require('./chainResult');
 module.exports = function (list, callback) {
     var result = [];
 
-    if (!list || !callback || !isFunction(callback)) {
-        return chainResult(this, result);
-    }
+    if (list && callback && isFunction(callback)) {
 
-    if (!list.length) {
-        return chainResult(this, [].concat(list));
-    }
-
-    if (list instanceof Array) {
-        return chainResult(this, list.map(callback));
-    }
-
-    for (var i = 0; i < list.length; i++) {
-        var item = list[i];
-
-        result.push(callback(item, i) || item);
+        if (!list.length) {
+            result = [].concat(list);
+        } else if (list instanceof Array) {
+            result = list.map(callback);
+        } else {
+            for (var i = 0; i < list.length; i++) {
+                var item = list[i];
+                result.push(callback(item, i) || item);
+            }
+        }
+        
     }
 
     return chainResult(this, result);
